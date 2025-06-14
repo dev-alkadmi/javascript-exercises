db0c20bfb0611fadf565e6f4c27803cd5f80830c
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-14 14:33:07 -0400

    created removed from array function that takes in any number of arguments and removes them from inputed array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputtedArray) {
+    const args = Array.from(arguments);
+    for (let index = 1; index < args.length; index++) {
+        let i = 0;
+        while (i < inputtedArray.length) {
+            if (inputtedArray[i] === args[index]) {
+                inputtedArray.splice(i, 1);
+            }
+            else {
+                ++i;
+            }
+        }
+    }
+
+    return inputtedArray;
 };
 
 // Do not edit below this line
