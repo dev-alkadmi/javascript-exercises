d7a8aa1dd99a3e6d17ff438610f9649c1b0f26bf
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-14 13:25:49 -0400

    created repeatString function that takes in a string and a number and repeats a word a number of times equal to the inputed number

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function (word, numberOfRepeats) {
+    let repeatedWord = word;
+
+    if (numberOfRepeats == 0) {
+        repeatedWord = '';
+    }
+    else if(numberOfRepeats < 0){
+        return "ERROR";
+    }
+    else {
+        for (let index = 1; index < numberOfRepeats; index++) {
+            repeatedWord += word;
+        }
+    }
+
+    return repeatedWord;
 
 };
 
