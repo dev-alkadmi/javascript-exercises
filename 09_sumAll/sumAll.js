92cf61cf107e502e06b556735b30340392afa97d
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-14 14:55:32 -0400

    created sumAll function that add all the numbers between 2 positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, lastNumber) {
+    if (firstNumber >= 0 && lastNumber >= 0 && Number.isInteger(firstNumber) && Number.isInteger(lastNumber)) {
+        let biggerNumber;
+        let smallerNumber;
 
+        if (firstNumber >= lastNumber) {
+            biggerNumber = firstNumber;
+            smallerNumber = lastNumber
+        }
+        else {
+            biggerNumber = lastNumber;
+            smallerNumber = firstNumber;
+        }
+
+        return ((biggerNumber - smallerNumber + 1) / 2) * (biggerNumber + smallerNumber); //function to add all numbers between the inputted numbers
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
