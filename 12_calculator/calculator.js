dd943d8719cad4e5a310ce822d985f38b5466585
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-17 16:52:56 -0400

    added functionality to multiply function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,30 +1,40 @@
-const add = function(num1, num2) {
-	return num1 + num2;
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function(num1, num2) {
-	return num1 - num2;
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function(sumArray) {
-	let total = 0;
+const sum = function (sumArray) {
+  let total = 0;
   for (const num of sumArray) {
-    total+= num;
+    total += num;
   }
 
   return total;
 };
 
-const multiply = function() {
+const multiply = function (multiplyArray) {
+  let total = 0;
+  for (let index = 0; index < multiplyArray.length; index++) {
+    if (index == 0) {
+      total = multiplyArray[0];
+    }
+    else {
+      total *= multiplyArray[index];
+    }
+  }
+
+  return total;
+};
+
+const power = function () {
 
 };
 
-const power = function() {
-	
-};
+const factorial = function () {
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
