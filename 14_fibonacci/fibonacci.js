7fdda36fff4a713708f5a9130a25c879960e3cc6
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-18 13:08:31 -0400

    created logic for fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = Number(num);
+    
+    if (num < 0) {
+        return "OOPS";
+    }
+    else if (num == 0) {
+        return 0;
+    }
+    else if (num == 1 || num == 2) {
+        return 1;
+    }
 
+    let fibonacciArray = [1, 1];
+
+    for (let index = 2; index < num; index++) {
+        fibonacciArray.push(fibonacci(index) + fibonacci(index - 1));
+
+    }
+
+    return fibonacciArray[num - 1];
 };
 
 // Do not edit below this line
