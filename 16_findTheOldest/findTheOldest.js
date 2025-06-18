0c52befeb527ff38445f100b4674a6794990b0cf
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-18 13:35:48 -0400

    added functionality to find oldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function (deadPeopleArray) {
+    let oldestPerson;
+    let oldestPersonAge;
+    let currentPersonAge;
+
+    for (let index = 0; index < deadPeopleArray.length; index++) {
+        if (index == 0) {
+            if (!('yearOfDeath' in deadPeopleArray[index])) {
+                oldestPerson = deadPeopleArray[index];
+                oldestPersonAge = new Date().getFullYear() - deadPeopleArray[index].yearOfBirth;
+            }
+            else {
+                oldestPerson = deadPeopleArray[index];
+                oldestPersonAge = deadPeopleArray[index].yearOfDeath - deadPeopleArray[index].yearOfBirth;
+            }
+        }
+        else {
+            if (!('yearOfDeath' in deadPeopleArray[index])) {
+                currentPersonAge = new Date().getFullYear() - deadPeopleArray[index].yearOfBirth;
+                if (currentPersonAge > oldestPersonAge) {
+                    oldestPerson = deadPeopleArray[index];
+                    oldestPersonAge = currentPersonAge
+                }
+            }
+            else {
+                currentPersonAge = deadPeopleArray[index].yearOfDeath - deadPeopleArray[index].yearOfBirth;
+                if (currentPersonAge > oldestPersonAge) {
+                    oldestPerson = deadPeopleArray[index];
+                    oldestPersonAge = currentPersonAge
+                }
+            }
+        }
+    }
+
+    return oldestPerson;
 
 };
 
