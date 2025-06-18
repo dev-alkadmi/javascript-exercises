321acba5899a80a382d34128513b8a52ed86dafb
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-18 11:50:19 -0400

    created logic for palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const noPunctuationPhrase = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g,'').toLowerCase();
+    const reversedNoPunctuationPhrase = noPunctuationPhrase.split("").reverse().join("").toLowerCase();
+    return reversedNoPunctuationPhrase == noPunctuationPhrase;
 };
 
 // Do not edit below this line
