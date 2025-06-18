8a945dc848dd25ff9b716e54a6a4c102165c4699
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-18 13:15:03 -0400

    added functionality to getTheTitles function

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    let bookNameArray = [];
 
+    for (let index = 0; index < bookArray.length; index++) {
+        bookNameArray.push(bookArray[index].title);
+    }
+
+    return bookNameArray;
 };
 
 // Do not edit below this line
